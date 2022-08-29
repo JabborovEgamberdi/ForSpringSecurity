d1220a2853380e1c5a0ce72942544f7ebf6c4472
JabborovEgamberdi <jabborovegamberdi2002@gmail.com>
2022-08-29 22:57:20 +0500

    classes created

---
+++ app-rest-jwt/src/main/java/uz/pdp/apprestjwt/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package uz.pdp.apprestjwt.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import uz.pdp.apprestjwt.services.MyAuthService;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final MyAuthService myAuthService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(myAuthService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/api/auth/login").permitAll()
+                .anyRequest().authenticated();
+    }
+}
