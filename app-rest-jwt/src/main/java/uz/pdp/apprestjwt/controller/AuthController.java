622f6a34d879ad2b6b6b03b5e20e800012b737bf
JabborovEgamberdi <jabborovegamberdi2002@gmail.com>
2022-08-30 17:40:18 +0500

    classes created

---
+++ app-rest-jwt/src/main/java/uz/pdp/apprestjwt/controller/AuthController.java
@@ -0,0 +1,35 @@
+package uz.pdp.apprestjwt.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.pdp.apprestjwt.payload.LoginDTO;
+import uz.pdp.apprestjwt.security.JwtProvider;
+import uz.pdp.apprestjwt.services.MyAuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final MyAuthService myAuthService;
+
+    private final JwtProvider jwtProvider;
+
+    @PostMapping("/login")
+    public HttpEntity<?> loginToSystem(@RequestBody LoginDTO loginDTO) {
+        UserDetails userDetails = myAuthService.loadUserByUsername(loginDTO.getPassword());
+        boolean existUser = userDetails.getPassword().equals(loginDTO.getPassword());
+        if (existUser) {
+            String token = jwtProvider.generateToken(loginDTO.getUsername());
+            return ResponseEntity.ok(token);
+        }
+        return ResponseEntity.status(401).body("Login yoki parol xato");
+    }
+}
