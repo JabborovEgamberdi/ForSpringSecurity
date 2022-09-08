68514c0dc809a689a2e70c4b16301b8af5bf7125
JabborovEgamberdi <jabborovegamberdi2002@gmail.com>
2022-09-08 18:39:10 +0500

    some classes updated

--- app-rest-jwt/src/main/java/uz/pdp/apprestjwt/config/JwtFilter.java
+++ app-rest-jwt/src/main/java/uz/pdp/apprestjwt/config/JwtFilter.java
@@ -1,7 +1,9 @@
 package uz.pdp.apprestjwt.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import uz.pdp.apprestjwt.security.JwtProvider;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -12,12 +14,23 @@ import java.io.IOException;
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
+    @Autowired
+    JwtProvider jwtProvider;
+
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest,
                                     HttpServletResponse httpServletResponse,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        String authorization = httpServletRequest.getHeader("Authorization");
-        System.out.println(authorization);
+//        String token = httpServletRequest.getHeader("Authorization");
+//        System.out.println("token = " + token);
+//        if (token != null && token.startsWith("Bearer")) {
+//            token = token.substring(7);
+//            boolean validateToken = jwtProvider.validateToken(token);
+//            if (validateToken) {
+//                String username = jwtProvider.getUsernameFromToken(token);
+//                System.out.println(username);
+//            }
+//        }
     }
 }
