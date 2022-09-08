f82e0dcca5f4b5956040ca0a49f7e6a47706bf2d
JabborovEgamberdi <jabborovegamberdi2002@gmail.com>
2022-09-08 18:40:39 +0500

    some classes updated

--- app-rest-jwt/src/main/java/uz/pdp/apprestjwt/services/MyAuthService.java
+++ app-rest-jwt/src/main/java/uz/pdp/apprestjwt/services/MyAuthService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import uz.pdp.apprestjwt.config.JwtFilter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
