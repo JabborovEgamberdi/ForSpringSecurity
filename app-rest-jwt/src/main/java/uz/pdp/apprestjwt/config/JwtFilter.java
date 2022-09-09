e7bb201abdf9019863afd3648ffa050f77cf2fb3
JabborovEgamberdi <jabborovegamberdi2002@gmail.com>
2022-09-09 18:44:23 +0500

    some classes updated

--- app-rest-jwt/src/main/java/uz/pdp/apprestjwt/config/JwtFilter.java
+++ app-rest-jwt/src/main/java/uz/pdp/apprestjwt/config/JwtFilter.java
@@ -1,9 +1,11 @@
 package uz.pdp.apprestjwt.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import uz.pdp.apprestjwt.security.JwtProvider;
+import uz.pdp.apprestjwt.services.MyAuthService;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -17,20 +19,27 @@ public class JwtFilter extends OncePerRequestFilter {
     @Autowired
     JwtProvider jwtProvider;
 
+    @Autowired
+    MyAuthService myAuthService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest,
                                     HttpServletResponse httpServletResponse,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-//        String token = httpServletRequest.getHeader("Authorization");
+        String token = httpServletRequest.getHeader("Authorization");
 //        System.out.println("token = " + token);
-//        if (token != null && token.startsWith("Bearer")) {
-//            token = token.substring(7);
-//            boolean validateToken = jwtProvider.validateToken(token);
-//            if (validateToken) {
+        if (token != null && token.startsWith("Bearer")) {
+            token = token.substring(7);
+            boolean validateToken = jwtProvider.validateToken(token);
+            if (validateToken) {
+                String username = jwtProvider.getUsernameFromToken(token);
+                UserDetails userDetails = myAuthService.loadUserByUsername(username);
+
+
 //                String username = jwtProvider.getUsernameFromToken(token);
 //                System.out.println(username);
-//            }
-//        }
+            }
+        }
     }
 }
